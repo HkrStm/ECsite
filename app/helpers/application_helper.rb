b462ab1c3b020845143035b7757134c45fafd963
HikaruSotome <polebear111@yahoo.co.jp>
2022-02-28 15:10:27 +0900

    head、header、footerの共通化とHomeへのリンクを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  BASE_TITLE = "BIGBAG Store"
+
+  def full_title(page_title)
+    page_title.blank? ? BASE_TITLE : "#{page_title} - #{BASE_TITLE}"
+  end
 end
