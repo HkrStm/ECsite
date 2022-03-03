7044f1956b168e89054159846dcc97137b31db7f
HikaruSotome <polebear111@yahoo.co.jp>
2022-03-03 10:04:50 +0900

    製品データ取得テスト実装

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,12 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
   describe "#show" do
-    let!(:product) { create(:product) }
+    let(:product) { create(:product) }
 
-    it "正常にレスポンスを返すこと" do
+    before do
       get potepan_product_path(product.id)
+    end
+
+    it "リクエストが成功すること" do
       expect(response).to be_successful
       expect(response).to have_http_status(200)
+    end
+
+    it "データを取得できていること" do
+      expect(response.body).to include product.name
+      expect(response.body).to include product.display_price.to_s
+      expect(response.body).to include product.description
     end
   end
 end
