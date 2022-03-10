5bfc01b8afe24db7dc520c61f4f8884cd667b9bb
HikaruSotome <polebear111@yahoo.co.jp>
2022-03-10 21:11:27 +0900

    カテゴリー内の商品一覧実装

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,6 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
-    @taxonomies = Spree::Taxon.roots
+    @taxonomies = Spree::Taxonomy.includes(:root)
+    @taxon_products = Spree::Product.in_taxons(@taxon).includes(master: [:default_price, images: [attachment_attachment: :blob]])
   end
 end
