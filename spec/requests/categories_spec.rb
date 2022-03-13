a371303e7ef401377e2c98158a007fc12bd3b755
HikaruSotome <polebear111@yahoo.co.jp>
2022-03-13 03:21:47 +0000

    不要な記述を削除

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "Categories", type: :request do
     it "データを取得できていること" do
       expect(response.body).to include product.name
       expect(response.body).to include product.display_price.to_s
-      expect(response.body).to include "#{image.filename}"
+      expect(response.body).to include image.filename
     end
   end
 end
