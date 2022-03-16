c14c91fd5ef371783dc9b1acd6f32cd8963c861b
HikaruSotome <polebear111@yahoo.co.jp>
2022-03-17 04:06:13 +0900

    商品詳細ページ中の関連商品表示部分を動的表示

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,11 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
+    @related_products = Spree::Product.
+      in_taxons(@product.taxons).
+      includes(master: [:default_price, images: [attachment_attachment: :blob]]).
+      where.not(id: @product.id).
+      distinct.
+      max(4)
   end
 end
