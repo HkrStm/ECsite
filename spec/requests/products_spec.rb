fa6ad64e4f605dcc98b99659968c00c772aa9f3c
HikaruSotome <polebear111@yahoo.co.jp>
2022-03-17 05:40:22 +0900

    関連商品のrequests specを実装

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
   describe "#show" do
-    let(:taxon)   { create(:taxon) }
-    let(:product) { create(:product, taxons: [taxon]) }
-
+    let(:taxon)           { create(:taxon) }
+    let(:product)         { create(:product, taxons: [taxon]) }
+    let(:related_product) { create(:product, taxons: [taxon]) }
+    let(:image)           { create(:image) }
+    let(:other_image)     { create(:image) }
+  
     before do
+      product.images         << image
+      related_product.images << other_image
       get potepan_product_path(product.id)
     end
 
@@ -13,10 +18,17 @@ RSpec.describe "Products", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it "データを取得できていること" do
+    it "商品のデータを取得できていること" do
       expect(response.body).to include product.name
       expect(response.body).to include product.display_price.to_s
       expect(response.body).to include product.description
+      expect(response.body).to include image.filename
+    end
+
+    it "関連商品のデータを取得できていること" do
+      expect(response.body).to include related_product.name
+      expect(response.body).to include related_product.display_price.to_s
+      expect(response.body).to include other_image.filename
     end
   end
 end
