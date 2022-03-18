2d51c0ed85709fa38d275e7f20b79974d929302b
HikaruSotome <polebear111@yahoo.co.jp>
2022-03-19 06:48:41 +0900

    関連商品のテストを実装

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,15 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
   describe "#show" do
-    let(:taxon)           { create(:taxon) }
-    let(:product)         { create(:product, taxons: [taxon]) }
-    let(:related_product) { create(:product, taxons: [taxon]) }
-    let(:image)           { create(:image) }
-    let(:other_image)     { create(:image) }
+    let(:taxonomy)         { create(:taxonomy) }
+    let(:root)             { taxonomy.root }
+    let(:taxon)            { create(:taxon, taxonomy: taxonomy, parent: root) }
+    let(:other_taxon)      { create(:taxon, taxonomy: taxonomy, parent: root) }
+    let(:product)          { create(:product, taxons: [taxon], price: 11) }
+    let(:other_product)    { create(:product, taxons: [other_taxon], price: 15) }
+    let(:related_products) { create_list(:product, 5, taxons: [taxon]) }
+    let(:image)            { create(:image) }
 
     before do
-      product.images         << image
-      related_product.images << other_image
+      product.images << image
+      related_products.each do |related_product|
+        related_product.images << create(:image)
+      end
       get potepan_product_path(product.id)
     end
 
@@ -25,10 +30,15 @@ RSpec.describe "Products", type: :request do
       expect(response.body).to include image.filename
     end
 
-    it "関連商品のデータを取得できていること" do
-      expect(response.body).to include related_product.name
-      expect(response.body).to include related_product.display_price.to_s
-      expect(response.body).to include other_image.filename
+    it "関連商品のデータを最大4つまで取得できていること" do
+      related_products[1..4].all? do |related_product|
+        expect(response.body).to include related_product.name
+      end
+      expect(response.body).not_to include related_products[0].name
+    end
+
+    it "関連した商品以外のデータは取得されないこと" do
+      expect(response.body).not_to include other_product.name
     end
   end
 end
--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Products", type: :system do
-  let(:taxonomy)               { create(:taxonomy) }
-  let(:root)                   { taxonomy.root }
-  let(:taxon)                  { create(:taxon, taxonomy: taxonomy, parent: root) }
-  let(:other_taxon)            { create(:taxon, taxonomy: taxonomy, parent: root) }
-  let(:product)                { create(:product, taxons: [taxon], price: 10) }
-  let(:other_product)          { create(:product, taxons: [other_taxon], price: 15) }
-  let(:related_products)       { create(:product, taxons: [taxon]) }
-  let(:image)                  { create(:image) }
-  let(:other_product_image)    { create(:image) }
-  let(:related_products_image) { create(:image) }
+  let(:taxonomy)            { create(:taxonomy) }
+  let(:root)                { taxonomy.root }
+  let(:taxon)               { create(:taxon, taxonomy: taxonomy, parent: root) }
+  let(:other_taxon)         { create(:taxon, taxonomy: taxonomy, parent: root) }
+  let(:product)             { create(:product, taxons: [taxon], price: 11) }
+  let(:other_product)       { create(:product, taxons: [other_taxon], price: 15) }
+  let(:related_products)    { create_list(:product, 5, taxons: [taxon]) }
+  let(:image)               { create(:image) }
+  let(:other_product_image) { create(:image) }
 
   before do
-    product.images       << image
-    other_product.images << other_product_image
-    related_products.images << related_products_image
+    product.images << image
+    related_products.each do |related_product|
+      related_product.images << create(:image)
+    end
     visit potepan_product_path(product.id)
   end
 
@@ -35,29 +35,33 @@ RSpec.describe "Products", type: :system do
 
   describe "関連商品" do
     scenario "関連商品をクリックしてその商品詳細ページへ移動する" do
-      click_on related_products.name
-      expect(current_path).to eq potepan_product_path(related_products.id)
+      click_on related_products[1].name
+      expect(current_path).to eq potepan_product_path(related_products[1].id)
     end
 
     it "関連商品が表示されること" do
-      expect(page).to have_content related_products.name
-      expect(page).to have_content related_products.display_price
-      expect(page).to have_selector("img, [src$='#{related_products_image.filename}']")
+      related_products[1..4].all? do |related_product|
+        expect(page).to have_content related_product.name
+        expect(page).to have_content related_product.display_price
+      end
     end
 
-    it "関連商品以外は表示されないこと" do
+    it "関連した商品以外は関連商品に表示されないこと" do
       expect(page).not_to have_content other_product.name
       expect(page).not_to have_content other_product.display_price
     end
 
     it "商品詳細に表示されている商品が関連商品に表示されないこと" do
-      within '.productBox' do
+      within '.productsContent' do
         expect(page).not_to have_content product.name
         expect(page).not_to have_content product.display_price
       end
     end
 
-    xit "関連商品が最大4つ表示されること" do
+    it "関連商品が最大4つまで表示されること" do
+      within '.productsContent' do
+        expect(page.all('.productBox').count).to eq 4
+      end
     end
   end
 end
