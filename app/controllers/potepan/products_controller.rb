3ef108571c6018c8136b689f98cfafb960be8746
HikaruSotome <polebear111@yahoo.co.jp>
2022-03-20 10:21:25 +0900

    汎用性のため定数とlimitメソッドをコントローラに移動

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,8 +1,11 @@
 class Potepan::ProductsController < ApplicationController
+  LIMIT_OF_RELATED_PRODUCTS = 4
+
   def show
     @product = Spree::Product.find(params[:id])
     @related_products = @product.
       related_products.
-      includes(master: [:default_price, images: [attachment_attachment: :blob]])
+      includes(master: [:default_price, images: [attachment_attachment: :blob]]).
+      limit(LIMIT_OF_RELATED_PRODUCTS)
   end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,12 +1,9 @@
 module Spree::ProductDecorator
-  LIMIT_OF_RELATED_PRODUCTS = 4
-
   def related_products
     Spree::Product.
       in_taxons(taxons).
       where.not(id: id).
-      distinct.
-      limit(LIMIT_OF_RELATED_PRODUCTS)
+      distinct
   end
 
   Spree::Product.prepend self
