16cf8ca650b0428a8a17f7d41dfba4970f7a9819
HikaruSotome <polebear111@yahoo.co.jp>
2022-03-20 14:06:15 +0900

    定数名を変更

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,11 +1,11 @@
 class Potepan::ProductsController < ApplicationController
-  LIMIT_OF_RELATED_PRODUCTS = 4
+  MAX_RELATED_PRODUCTS_COUNT = 4
 
   def show
     @product = Spree::Product.find(params[:id])
     @related_products = @product.
       related_products.
       includes(master: [:default_price, images: [attachment_attachment: :blob]]).
-      limit(LIMIT_OF_RELATED_PRODUCTS)
+      limit(MAX_RELATED_PRODUCTS_COUNT)
   end
 end
